c7ad785b9c38c6a22ef1d726f5172b83c6b827b5
gamec71 <gamec71@users.noreply.github.com>
2019-03-30 11:37:06 +0800

    UI框架学习原模板
    所有初始源代码

---
+++ Assets/Scripts/DemoProject/SelectHeroUIForm.cs
@@ -0,0 +1,56 @@
+﻿/***
+ * 
+ *    Title: "SUIFW" UI框架项目
+ *           主题： PRG 游戏“选择角色”窗体 
+ *    Description: 
+ *           功能： 选择英雄窗体
+ *                  
+ *    Date: 2017
+ *    Version: 0.1版本
+ *    Modify Recoder: 
+ *    
+ *   
+ */
+using System.Collections;
+using System.Collections.Generic;
+using SUIFW;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DemoProject
+{
+    public class SelectHeroUIForm : BaseUIForm
+    {
+
+        public void Awake()
+        {
+            //Log.SyncLogCatchToFile();   xy临时注销
+            //窗体的性质
+            CurrentUIType.UIForms_ShowMode = UIFormShowMode.HideOther;
+
+            //注册进入主城的事件
+            RigisterButtonObjectEvent("BtnConfirm",
+                p =>
+                {
+                    OpenUIForm(ProConst.MAIN_CITY_UIFORM);
+                    OpenUIForm(ProConst.HERO_INFO_UIFORM);
+                }
+
+                );
+
+            //注册返回上一个页面
+            RigisterButtonObjectEvent("BtnClose",
+                m => CloseUIForm()
+                );
+
+            //GameObject goButton = UnityHelper.FindTheChildNode(this.gameObject, "BtnClose").gameObject;
+            //goButton.GetComponent<Button>().onClick.AddListener(M);
+        }
+
+        //void M()
+        //{
+        //    Debug.Log("aaa");
+        //    CloseUIForm();
+        //}
+    }
+}
